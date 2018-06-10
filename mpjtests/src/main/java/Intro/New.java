70c0b4934fd9fc25a6c03622700987dd26d69aca
MrErei <deejtonks@gmail.com>
2018-06-10 15:22:03 +1200

    assignment

--- mpjtests/src/main/java/Intro/New.java
+++ mpjtests/src/main/java/Intro/New.java
@@ -7,10 +7,12 @@ import java.util.*;
 public class New {
 
     //Constants
-    private static final int MAX_TRAINING = 135;
-    private static final int MAX_TESTING = 15;
-    private static final int ROOT = 0;
-    private static final int MAX_NEIGH = 3;
+    private static final int MAX_LINES = 150;
+    private static final int MAX_TESTING = 15; //The number of test cases
+    private static final int MAX_TRAINING = MAX_LINES - MAX_TESTING; //# of training
+
+    private static final int ROOT = 0; //The root process
+    private static final int MAX_NEIGH = 3; //In the assignment doc, this is K
 
     public static void main(String[] args) throws Exception {
 
@@ -38,15 +40,13 @@ public class New {
         //The final result gathered back to the main thread (ROOT)
         Object[] out = new Object[MAX_TESTING*size];
 
-
-
-        /*
+        /*==============================================================================================================
             The main process/ROOT will load the data set,
-            selecting 15 random test cases and fetching the
+            selecting 15 random test cases and fetch the
             remainder 135 to form the training set.
 
             The test cases are then broadcasted to all processes.
-         */
+        ==============================================================================================================*/
         if(rank == ROOT) {
 
             Loader.load(training, testing, "iris.data");
@@ -60,27 +60,33 @@ public class New {
         }
 
 
-        //SCATTER the training set across all processes evenly
+        /*==============================================================================================================
+            1. Scatter the training set across all threads.
+            2. For each test case, compute all distances.
+            3. Return a SortedMap, consisting of K distances, and the original test case at the end.
+            Minimizes the communication across the threads.
+        ==============================================================================================================*/
+
         //WARNING let n = # threads, then sendSize will be some float. Not all will be sent.
-        System.out.println(sendSize);
         MPI.COMM_WORLD.Scatter(training, 0, sendSize, MPI.OBJECT, localTrain, 0, sendSize, MPI.OBJECT, ROOT);
 
-        long start = System.currentTimeMillis();
-
-        /*
-            Now, for every test case,
-            compute the distances for every training vector, to the test case and
-            then add it into a sorted list.
-         */
+        //===
         List<SortedMap<Double, String>> result = new ArrayList<>();
         for(String[] test : testing) {
 
 
+            //This will store all the distances for a particular test case
             SortedMap<Double, String> distances = new TreeMap<>();
 
             for(String[] train : localTrain) {
 
                 try {
+
+                    /*
+                        I'm aware this can be thrown into a loop, it just looks more logical.
+                        sqrt((train[0] - test[0])^2 + ... + (train[3] - test[3])^2))
+                        Pre-parsing the floats would be more time consuming
+                     */
                     distances.put(
                             Math.sqrt(
                                     Math.pow(Float.parseFloat(train[0]) - Float.parseFloat(test[0]), 2) +
@@ -91,15 +97,21 @@ public class New {
                     );
                 }
                 catch(NumberFormatException | NullPointerException | ArrayIndexOutOfBoundsException ex) {
+                    //None of these issues should arise
                     ex.printStackTrace();
                 }
             }
 
+            /*
+                GENERATING THE RESULT FOR EACH THREAD
+             */
             //Only take the top K for this process: minimizing what needs to be sent
             SortedMap<Double, String> top = new TreeMap<>();
-            //Put the test case at the front, just the class
-            double key = Float.parseFloat(test[0]) + Float.parseFloat(test[1]) + Float.parseFloat(test[2]) + Float.parseFloat(test[3]);
-            top.put(key, test[4]);
+
+            //Put the test case at the end (it's a sorted map), testKey := a unique key representing the test case
+            double testKey = Float.parseFloat(test[0]) + Float.parseFloat(test[1]) + Float.parseFloat(test[2]) + Float.parseFloat(test[3]);
+            top.put(testKey, test[4]);
+
             for(Map.Entry<Double, String> entry : distances.entrySet()) {
                 if(top.size() - 1 >= MAX_NEIGH) break;
 
@@ -109,17 +121,22 @@ public class New {
         }
 
 
+        //Convert to an array for sending
         Object[] array = result.toArray();
 
-        long end = System.currentTimeMillis();
-        System.out.println(end-start);
+        /*==============================================================================================================
+            1. Gather the results from all processes.
+            2. The root process shall join the results from each process into their test case.
+            3. Each test case will then have their distances sorted and trimmed to the top MAX_NEIGH (K).
+            4. The majority or class will be returned for that test case.
+        ==============================================================================================================*/
+
         MPI.COMM_WORLD.Gather(array, 0, array.length, MPI.OBJECT, out, 0, array.length, MPI.OBJECT, ROOT);
 
 
 
         if(rank == ROOT) {
 
-            start = System.currentTimeMillis();
             List<SortedMap<Double, String>> fin = new ArrayList<>();
 
             for(int i = 0; i < out.length; i++) {
@@ -148,14 +165,16 @@ public class New {
 
             }
 
+            System.out.println("TEST CASES");
+            for(String[] test : testing) {
+                double testKey =
+                        Float.parseFloat(test[0]) + Float.parseFloat(test[1]) + Float.parseFloat(test[2]) + Float.parseFloat(test[3]);
+                System.out.println(test[4] + ", " + testKey);
+            }
             for(SortedMap<Double, String> test : fin) {
                 System.out.println(test);
             }
 
-
-            end = System.currentTimeMillis();
-            System.out.println(end-start);
-            System.out.println("done");
         }
     }
 }
